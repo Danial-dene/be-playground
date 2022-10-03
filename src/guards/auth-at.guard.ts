dd78b4a9f30a2f1527206963de33a16e1b22e364
Sirazulhaq Bumpy <sirazulhaq0205@gmail.com>
2022-10-03 17:04:37 +0800

    delete more nothing

--- src/guards/auth-at.guard.ts
+++ src/guards/auth-at.guard.ts
@@ -8,7 +8,6 @@ export class AtGuard extends AuthGuard('at-jwt') {
   getRequest(context: ExecutionContext): unknown {
     const ctx = GqlExecutionContext.create(context);
     const req = ctx.getContext().req;
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-member-access
     const jwt = ExtractJwt.fromAuthHeaderAsBearerToken()(req);
     const decodedJwt = JWT.decode(jwt);
     req['user'] = decodedJwt;
